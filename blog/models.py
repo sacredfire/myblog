1daaee45e6f133c709df14717d57f7d4094a3a9c
Sergei Kissel <sergei.kissel@gmail.com>
2016-05-25 21:34:37 +0300

    migrated model blog io myblog

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Post(models.Model):
+    class Meta:
+        verbose_name = "post"
+        verbose_name_plural = "posts"
+    authour = models.ForeignKey('auth.User')
+    title = models.CharField(max_length=100)
+    text = models.TextField()
+    date_created = models.DateTimeField(default=timezone.now)
+    date_published = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.date_published = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
