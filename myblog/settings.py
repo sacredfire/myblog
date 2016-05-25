57c935264ba9489bb4d9ecb785709810a0c832b5
Sergei Kissel <sergei.kissel@gmail.com>
2016-05-26 00:04:10 +0300

    STATIC_ROOT

--- myblog/settings.py
+++ myblog/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 # STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 STATIC_URL = '/static/'
