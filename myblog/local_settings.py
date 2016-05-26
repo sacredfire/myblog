d4cd1849dadba243af5c36f3268c0018088d4ddc
Sergei Kissel <sergei.kissel@gmail.com>
2016-05-26 11:08:03 +0300

    edited local_settings - comented out db

--- myblog/local_settings.py
+++ myblog/local_settings.py
@@ -5,11 +5,11 @@ SECRET_KEY = '+2dto*1_pr&vq2w3)jr2&9e-lt#idvyfcmtaq1ob0uidgz%w9c'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
-DEBUG = True
+# DEBUG = True
