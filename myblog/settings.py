348452774dedaed098000659797fc5dfe1ee98f8
Sergei Kissel <sergei.kissel@gmail.com>
2016-05-26 22:55:10 +0300

    added view and html template

--- myblog/settings.py
+++ myblog/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '+2dto*1_pr&vq2w3)jr2&9e-lt#idvyfcmtaq1ob0uidgz%w9c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
